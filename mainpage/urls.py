9ea757fd5b70a79427637ea52b5099b511d326cc
Aktparihar <ankitpariharmiku@gmail.com>
2021-02-01 19:09:15 +0530

    registration and login page and database added

--- mainpage/urls.py
+++ mainpage/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index,Register_view,Login_view
 
 urlpatterns = [
     path('', index),
+    path('register/', Register_view),
+    path('login/', Login_view),
 ]
--- mainpage/views.py
+++ mainpage/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html',{})
+
+def Login_view(request):
+    if request.method == 'POST':
+        user = authenticate(username=request.POST.get('email'), password=request.POST.get('password'))
+        if user is not None:
+            return render(request, 'MainPageAfterLogin.html')
+        else:
+            return render(request, 'login.html')
+    else:
+        return render(request, 'login.html')
+
+def Register_view(request):
+    if request.method == 'POST':
+        user = User.objects.create_user(request.POST.get('name'), request.POST.get('email'), request.POST.get('password'))
+        user.is_active = False
+        user.first_name = request.POST.get('name')
+        user.save()
+        return render(request, 'MainPageAfterLogin.html')
+    else:
+        return render(request, 'register.html')
